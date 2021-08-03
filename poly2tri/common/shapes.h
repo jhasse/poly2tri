1a38e2205d9c0617d0b91e8308095a790c73faf0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-08-03 16:25:20 -0400

    Clear shadow warning in initializer

--- poly2tri/common/shapes.h
+++ poly2tri/common/shapes.h
@@ -57,7 +57,7 @@ struct Point {
   std::vector<Edge*> edge_list;
 
   /// Construct using coordinates.
-  Point(double x, double y) : x(x), y(y) {}
+  Point(double i_x, double i_y) : x(i_x), y(i_y) {}
 
   /// Set this point to all zeros.
   void set_zero()
