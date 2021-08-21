136fa7acfc95cf06a3488102f0cff039b7f3485c
Jan Niklas Hasse <jhasse@bixense.com>
2021-08-21 17:28:47 +0200

    Move Point ctor into .cc file to hide shadowing from users
    See #15 and #31.

--- poly2tri/common/shapes.cc
+++ poly2tri/common/shapes.cc
@@ -35,6 +35,10 @@
 
 namespace p2t {
 
+Point::Point(double x, double y) : x(x), y(y)
+{
+}
+
 std::ostream& operator<<(std::ostream& out, const Point& point) {
   return out << point.x << "," << point.y;
 }
